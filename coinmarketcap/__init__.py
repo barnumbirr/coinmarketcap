c4bf03e2a6f39c8b5296e8bf41d7afd8b815142b
mondeja <mondejar1994@gmail.com>
2017-03-30 13:52:59 +0200

    Update __init__.py

--- coinmarketcap/__init__.py
+++ coinmarketcap/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__title__   = 'pymarketcap'
+__title__   = 'coinmarketcap'
 __version__ = '3.0.1'
-__author__ = 'Álvaro Mondéjar <mondejar1994@gmail.com>'
-__repo__    = 'https://github.com/mrsmn/pymarketcap'
+__author__ = 'Martin Simon <me@martinsimon.me>, Álvaro Mondéjar <mondejar1994@gmail.com>'
+__repo__    = 'https://github.com/mrsmn/coinmarketcap-api'
 __license__ = 'Apache v2.0 License'
 
 from .core import Market
