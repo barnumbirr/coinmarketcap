a8e10e3a0d330d2430f6e4422174a1159dbf84a3
Jath Palasubramaniam <jathpala@gmail.com>
2018-02-12 20:52:51 +1100

    Detect and handle symbol collisions
    As pointed out by @jorijnsmit, multiple coins can share the same
    symbol. Allowing such coins to be looked up by symbol would lead to
    ambiguity about exactly which coin is being requested.
    
    The 'get_symbols.py' script has been modified to detect such symbol
    collisions and exclude them from the symbol lookup dictionary.
    Attempting to lookup one of those symbols will lead to an error being
    raised (just as if such a symbol doesn't exist at all). Those coins
    with colliding symbols can still be looked up by id.
    
    When 'get_symbols.py' is run it creates a file 'collisions.log' which
    lists all collisions. It should be manually copied to 'docs/'.

--- scripts/get_symbols.py
+++ scripts/get_symbols.py
@@ -5,15 +5,37 @@ from coinmarketcap import Market
 import json
 
 __DATA_FILE = 'symbols.json'
+__COLLISION_LOG = 'collisions.log'
 
 if __name__ == "__main__":
 
     market = Market()
-    ticker = market.ticker(limit = 1000)
+    ticker = market.ticker(limit = 0)
 
     symbols = {}
+    collisions = {}
     for coin in ticker:
-        symbols[coin['symbol']] = coin['id']
+        if coin['symbol'] in symbols.keys():
+            # A coin with the given symbol already exists.
+            collisions[coin['symbol']] = [symbols[coin['symbol']], coin['id']]
+            del(symbols[coin['symbol']])
+        elif coin['symbol'] in collisions.keys():
+            # A collision for the given coin was already detected. This is a
+            # further collision.
+            collisions[coin['symbol']].append(coin['id'])
+        else:
+            # This is a new symbol.
+            symbols[coin['symbol']] = coin['id']
+
+    # Print all the symbols that had a collision.
+    collision_list = []
+    for symbol in collisions.keys():
+        ids = ", ".join(collisions[symbol])
+        print('Symbol collision -  %5s: %s.' % (symbol, ids))
+        collision_list.append('%5s: %s\n' % (symbol, ids))
 
     with open(__DATA_FILE, 'w') as fd:
         json.dump(symbols, fd, indent = 4, sort_keys = True)
+
+    with open(__COLLISION_LOG, 'w') as fd:
+        fd.writelines(collision_list)
