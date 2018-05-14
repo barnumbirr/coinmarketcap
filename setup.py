d9e200d10cc61d8ca428f616d9063c3778bf1fc2
Martin Simon <martin@examotive.com>
2018-05-14 10:54:54 +0200

    actually fix UnicodeDecodeError for long_description

--- setup.py
+++ setup.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# To use a consistent encoding
+from os import path
+from codecs import open
+# Always prefer setuptools over distutils
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name = 'coinmarketcap',
     packages = ['coinmarketcap'],
-    version = '5.0.1',
+    version = '5.0.2',
     description = 'Python wrapper around the coinmarketcap.com API.',
     author = 'Martin Simon',
     author_email = 'me@martinsimon.me',
     url = 'https://github.com/barnumbirr/coinmarketcap',
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/barnumbirr/coinmarketcap/issues',
+        'Funding': 'https://github.com/barnumbirr/coinmarketcap#buy-me-a-coffee',
+        'Source': 'https://github.com/barnumbirr/coinmarketcap',
+},
     license = 'Apache v2.0 License',
     install_requires=['requests==2.18.4', 'requests_cache==0.4.13'],
     keywords = ['cryptocurrency', 'API', 'coinmarketcap','BTC', 'Bitcoin', 'LTC', 'Litecoin', 'XRP', 'Ripple', 'ETH', 'Ethereum '],
@@ -26,6 +41,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    long_description = open('README.md','r').read(),
+    long_description = long_description,
     long_description_content_type='text/markdown',
 )
